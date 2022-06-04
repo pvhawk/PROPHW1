731f1e71532b0402aa0c56b1614a5a6b5c32a534
pvhawk <pvhawk87@gmail.com>
2022-06-04 23:24:38 +0600

    prop_homework_1_1-3

---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+if __name__ == '__main__':
+    print(datetime.date.today())
+    calculate_salary()
+    print(datetime.date.today())
+    get_employees()
